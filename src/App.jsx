91f1a8fb5ef4010e5a358d9d6cd526ea75b9846f
Allison Serrano <allisonserrano171@yahoo.com>
2023-10-30 20:11:43 -0400

    Signed In Web Page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div >
+        <p>You are now logged in! Please close this window and go back to the app.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
